345f66ef4d76ef8bddd0d6619aa8d453341cd052
Khushikam <94278433+Khushikam@users.noreply.github.com>
2021-12-04 18:47:01 +0530

    Add files via upload
    for loop

---
+++ scratch_canvas (1).py
@@ -0,0 +1,9 @@
+import turtle
+for i in range(0, 2):
+    turtle.pencolor('red')
+    turtle.forward(100)
+    turtle.left(90)
+for i in range(0, 2):
+    turtle.pencolor('green')
+    turtle.forward(100)
+    turtle.left(90)
